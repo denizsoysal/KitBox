c826447429dd9165afe209fbad4c75108872be6d
yannis97 <16133@ecam.be>
2019-03-13 10:00:47 +0100

    commit update interface client style

--- V1_KITBOX/V1_KITBOX/UC_Cart.Designer.cs
+++ V1_KITBOX/V1_KITBOX/UC_Cart.Designer.cs
@@ -42,44 +42,51 @@
             // labelResume
             // 
             this.labelResume.AutoSize = true;
-            this.labelResume.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelResume.Location = new System.Drawing.Point(36, 44);
+            this.labelResume.BackColor = System.Drawing.Color.LightGray;
+            this.labelResume.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResume.Font = new System.Drawing.Font("Calibri", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResume.Location = new System.Drawing.Point(72, 85);
+            this.labelResume.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelResume.Name = "labelResume";
-            this.labelResume.Size = new System.Drawing.Size(238, 39);
+            this.labelResume.Size = new System.Drawing.Size(330, 74);
             this.labelResume.TabIndex = 0;
-            this.labelResume.Text = "Votre panier :";
+            this.labelResume.Text = "Votre panier";
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(43, 115);
+            this.panel1.Location = new System.Drawing.Point(86, 221);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(563, 402);
+            this.panel1.Size = new System.Drawing.Size(1126, 773);
             this.panel1.TabIndex = 1;
             // 
             // labelPrixTotal
             // 
             this.labelPrixTotal.AutoSize = true;
-            this.labelPrixTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelPrixTotal.Location = new System.Drawing.Point(639, 364);
+            this.labelPrixTotal.Font = new System.Drawing.Font("Calibri", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrixTotal.Location = new System.Drawing.Point(1278, 700);
+            this.labelPrixTotal.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelPrixTotal.Name = "labelPrixTotal";
-            this.labelPrixTotal.Size = new System.Drawing.Size(108, 25);
+            this.labelPrixTotal.Size = new System.Drawing.Size(202, 53);
             this.labelPrixTotal.TabIndex = 2;
             this.labelPrixTotal.Text = "Prix total :";
             // 
             // labelDisponibilité
             // 
-            this.labelDisponibilité.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDisponibilité.Location = new System.Drawing.Point(606, 407);
+            this.labelDisponibilité.Font = new System.Drawing.Font("Calibri", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDisponibilité.Location = new System.Drawing.Point(1212, 783);
+            this.labelDisponibilité.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelDisponibilité.Name = "labelDisponibilité";
-            this.labelDisponibilité.Size = new System.Drawing.Size(153, 23);
+            this.labelDisponibilité.Size = new System.Drawing.Size(306, 44);
             this.labelDisponibilité.TabIndex = 3;
             this.labelDisponibilité.Text = "Disponibilité :";
             // 
             // buttonreturnShopping
             // 
-            this.buttonreturnShopping.Location = new System.Drawing.Point(612, 468);
+            this.buttonreturnShopping.Location = new System.Drawing.Point(1224, 900);
+            this.buttonreturnShopping.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonreturnShopping.Name = "buttonreturnShopping";
-            this.buttonreturnShopping.Size = new System.Drawing.Size(132, 49);
+            this.buttonreturnShopping.Size = new System.Drawing.Size(264, 94);
             this.buttonreturnShopping.TabIndex = 4;
             this.buttonreturnShopping.Text = "Continuer les achats";
             this.buttonreturnShopping.UseVisualStyleBackColor = true;
@@ -87,9 +94,10 @@
             // 
             // buttonValidateAndPay
             // 
-            this.buttonValidateAndPay.Location = new System.Drawing.Point(753, 468);
+            this.buttonValidateAndPay.Location = new System.Drawing.Point(1506, 900);
+            this.buttonValidateAndPay.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonValidateAndPay.Name = "buttonValidateAndPay";
-            this.buttonValidateAndPay.Size = new System.Drawing.Size(132, 49);
+            this.buttonValidateAndPay.Size = new System.Drawing.Size(264, 94);
             this.buttonValidateAndPay.TabIndex = 5;
             this.buttonValidateAndPay.Text = "Valider et payer";
             this.buttonValidateAndPay.UseVisualStyleBackColor = true;
@@ -98,9 +106,10 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(768, 366);
+            this.label1.Location = new System.Drawing.Point(1536, 704);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 23);
+            this.label1.Size = new System.Drawing.Size(224, 44);
             this.label1.TabIndex = 6;
             this.label1.Text = "90€";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -108,9 +117,10 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(748, 409);
+            this.label2.Location = new System.Drawing.Point(1496, 787);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 23);
+            this.label2.Size = new System.Drawing.Size(264, 44);
             this.label2.TabIndex = 7;
             this.label2.Text = "Dans une semaine";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -119,8 +129,9 @@
             // 
             this.buttonQuit.AutoSize = true;
             this.buttonQuit.Location = new System.Drawing.Point(0, 0);
+            this.buttonQuit.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonQuit.Name = "buttonQuit";
-            this.buttonQuit.Size = new System.Drawing.Size(128, 23);
+            this.buttonQuit.Size = new System.Drawing.Size(256, 44);
             this.buttonQuit.TabIndex = 8;
             this.buttonQuit.Text = "Vider le panier et quitter";
             this.buttonQuit.UseVisualStyleBackColor = true;
@@ -128,7 +139,7 @@
             // 
             // UC_Cart
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.Controls.Add(this.buttonQuit);
@@ -140,9 +151,9 @@
             this.Controls.Add(this.buttonValidateAndPay);
             this.Controls.Add(this.labelPrixTotal);
             this.Controls.Add(this.labelDisponibilité);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UC_Cart";
-            this.Size = new System.Drawing.Size(888, 554);
+            this.Size = new System.Drawing.Size(1776, 1065);
             this.Load += new System.EventHandler(this.UC_Cart_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
